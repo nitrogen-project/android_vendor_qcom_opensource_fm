ca23a817ddc9efb5e7e42e3cd4e44f70fb3135c5
cb <billchen1977@gmail.com>
2017-02-20 13:56:10 +0800

    Fix build with aosp

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE_TAGS := optional
 
 #ifneq ($(TARGET_USES_AOSP),true)
 
-#ifeq ($(BOARD_HAVE_QCOM_FM),true)
+ifeq ($(BOARD_HAVE_QCOM_FM),true)
 #ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
 
 LOCAL_SRC_FILES := $(call all-java-files-under, qcom/fmradio)
@@ -15,6 +15,14 @@ LOCAL_JNI_SHARED_LIBRARIES := libqcomfm_jni
 LOCAL_MODULE:= qcom.fmradio
 
 include $(BUILD_JAVA_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := qcom.fmradio.xml
+LOCAL_SRC_FILES := qcom/fmradio/$(LOCAL_MODULE)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT)/etc/permissions
+include $(BUILD_PREBUILT)
 
 ifeq ($(BOARD_HAS_QCA_FM_SOC), "cherokee")
 LOCAL_CFLAGS += -DFM_SOC_TYPE_CHEROKEE
@@ -26,6 +34,7 @@ include $(LOCAL_PATH)/fmapp2/Android.mk
 #LOCAL_PATH := $(LOCAL_DIR_PATH)
 #include $(LOCAL_PATH)/FMRecord/Android.mk
 
+ifeq ($(BOARD_HAS_QCA_FM_SOC), "cherokee")
 LOCAL_PATH := $(LOCAL_DIR_PATH)
 include $(LOCAL_PATH)/fm_hci/Android.mk
 
@@ -34,9 +43,10 @@ include $(LOCAL_PATH)/helium/Android.mk
 
 LOCAL_PATH := $(LOCAL_DIR_PATH)
 include $(LOCAL_PATH)/fmhalService/Android.mk
+endif
 
 #endif # is-vendor-board-platform
-#endif # BOARD_HAVE_QCOM_FM
+endif # BOARD_HAVE_QCOM_FM
 #endif # Not (TARGET_USES_AOSP)
 LOCAL_PATH := $(LOCAL_DIR_PATH)
 include $(LOCAL_PATH)/libfm_jni/Android.mk
--- jni/Android.mk
+++ jni/Android.mk
@@ -1,5 +1,5 @@
 ifeq ($(BOARD_HAVE_QCOM_FM),true)
-ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
+#ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -19,12 +19,14 @@ LOCAL_SHARED_LIBRARIES := \
         libnativehelper \
         libcutils
 
+ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+endif
 
 LOCAL_MODULE := libqcomfm_jni
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
-endif # is-vendor-board-platform
+#endif # is-vendor-board-platform
 endif # BOARD_HAVE_QCOM_FM
